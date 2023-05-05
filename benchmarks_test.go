6d78419105e792ebd263d6851dea3cd308cd41b3
wookmew <jiajung@opera.com>
2023-05-05 09:28:47 +0800

    chore: add doc and Default func

---
+++ benchmarks_test.go
@@ -0,0 +1,50 @@
+package meweb
+
+import (
+	"net/http"
+	"testing"
+)
+
+func BenchmarkOneRoute(B *testing.B) {
+	router := New()
+	router.GET("/ping", func(c *Context) {})
+	runRequest(B, router, "GET", "/ping")
+}
+
+type mockWriter struct {
+	headers http.Header
+}
+
+func newMockWriter() *mockWriter {
+	return &mockWriter{
+		http.Header{},
+	}
+}
+
+func (m *mockWriter) Header() http.Header {
+	return m.headers
+}
+
+func (m *mockWriter) Write(p []byte) (n int, err error) {
+	return len(p), nil
+}
+
+func (m *mockWriter) WriteString(s string) (n int, err error) {
+	return len(s), nil
+}
+
+func (m *mockWriter) WriteHeader(int) {}
+
+func runRequest(B *testing.B, r *Engine, method, path string) {
+	// create fake request
+	req, err := http.NewRequest(method, path, nil)
+	if err != nil {
+		panic(err)
+	}
+	w := newMockWriter()
+	B.ReportAllocs()
+	B.ResetTimer()
+	for i := 0; i < B.N; i++ {
+		r.ServeHTTP(w, req)
+	}
+}
---
+++ readme.md
@@ -0,0 +1,9 @@
+# meweb Web Framework
+
+meweb is a web framework demo written in [Go](https://go.dev/), study from [gin](https://github.com/gin-gonic/gin).
+
+# Examples  
+
+```
+
+```
